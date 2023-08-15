6af4b855ca156331a02ea784ae62805d2e641e41
med-aziz-guennichi <medazizguennichi@gmail.com>
2023-08-15 18:40:58 +0100

    fix close button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-import Api from './Aziz';
-
+import "./App.css";
+import Api from "./Aziz";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
 function App() {
   return (
     <div className="App">
-      <Api />
+      <Routes>
+        <Route path="/" element={<Api />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/Aziz.js
+++ src/Aziz.js
@@ -1,35 +1,67 @@
 import { JitsiMeeting } from "@jitsi/react-sdk";
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Api = () => {
+  const navigate = useNavigate();
 
+  const handleIframeLoad = () => {
+    const iframe = document.getElementById("jitsi-iframe");
+    if (iframe) {
+      iframe.onload = () => {
+        const anchorElement = iframe.contentDocument.querySelector(
+          "a.watermark.leftwatermark"
+        );
+        if (anchorElement) {
+          anchorElement.style.display = "none";
+        }
+      };
+    }
+  };
 
+  useEffect(() => {
+    handleIframeLoad();
+  }, []);
   return (
-    <>
     <JitsiMeeting
       domain="meet.jit.si"
       roomName="PleaseUseAGoodRoomName"
       configOverwrite={{
         enableClosePage: true,
+        enableWelcomePage: false,
+        // enable web cam for all users
+        enableVideo: true,
+        // i wanna to disable the logo
+
+        startWithAudioMuted: true,
+        disableModeratorIndicator: true,
+        startScreenSharing: true,
+        enableEmailInStats: false,
       }}
       interfaceConfigOverwrite={{
         DISABLE_JOIN_LEAVE_NOTIFICATIONS: false,
         WEBGL_DELETE_TEXTURE_THRESHOLD: false,
-        SHOW_PROMOTIONAL_CLOSE_PAGE:true
-    }}
+        SHOW_PROMOTIONAL_CLOSE_PAGE: true,
+        SHOW_JITSI_WATERMARK: false,
+        // HIDE_DEEP_LINKING_LOGO: true,
+      }}
       userInfo={{
-        displayName: 'aziz'
+        displayName: "aziz",
+        // what i can use in this ?
       }}
       onApiReady={(externalApi) => {
-        externalApi.removeAllListeners()
-        
+        externalApi.on("videoConferenceJoined", () => {
+          console.log("videoConferenceJoined");
+        });
+        externalApi.on("videoConferenceLeft", () => {
+          navigate("/home");
+        });
       }}
       getIFrameRef={(iframe) => {
-        iframe.style.height = '800px';
+        iframe.style.height = "750px";
+        iframe.id = "jitsi-iframe";
       }}
     />
-
-    </>
   );
 };
 
